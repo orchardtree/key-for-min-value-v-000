211e9077ee37ffc12a37de93b6334bdbb1625f7f
Jesse Wenger <wengerj@gmail.com>
2021-03-04 21:43:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,14 +4,18 @@ require 'pry'
 def key_for_min_value(name_hash)
   low_name = ""
   low_value = 0
-  name_hash.each do |name, value|
-    if low_name == "" && low_value = 0
-      low_name = name 
-      low_value = value 
-    elsif value < low_value
-      low_name = name 
-      low_value = value
+  if name_hash = {}
+    low_name = nil 
+  else
+    name_hash.each do |name, value|
+      if low_name == "" && low_value = 0
+        low_name = name 
+        low_value = value 
+      elsif value < low_value
+        low_name = name 
+        low_value = value
+      end
     end
-  end
+  end 
   low_name
 end
